16acea58d57ea852e91f6dd170368dcc8d075094
Aman Rawat <kirmolaa@gmail.com>
2024-05-21 09:29:45 +0530

    .

--- imager.py
+++ imager.py
@@ -6,14 +6,13 @@ from os import makedirs
 
 csv_file = r"data/Quotes.csv"
 
-file = pandas.read_csv(csv_file)[:5]
+file = pandas.read_csv(csv_file)
 
 
 def main():
     makedirs("generated_images", exist_ok=True)
 
     quotes = []
-    index = 1
     for each in zip(file["quote"], file["images"]):
         quotes.append(
             {
@@ -76,8 +75,6 @@ def main():
         thumbnail.save(f"generated_images/{quote["thumbnail"]}")
         image.close()
         thumbnail.close()
-        print(f"Created: {index}")
-        index += 1
 
 
 if __name__ == "__main__":
